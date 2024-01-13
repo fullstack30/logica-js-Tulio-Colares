210a1662d6bcc7020728cccddf97505c077bbad2
Tulio Colares <tuliocsaraiva@gmail.com>
2024-01-13 14:49:36 -0300

    add soma.js funcionando

--- src/soma.js
+++ src/soma.js
@@ -1,9 +1,24 @@
+// function soma(a, b) {
+//     if (typeof a === "string" || typeof b === "string"){
+//         return "parâmetros inválidos"
+//     } else if (typeof b === "undefined") {
+//         return "parâmetros inválidos"
+//     } else {
+//         return a + b
+//     }
+// }
+
+
 function soma(a, b) {
-    if (typeof a === "string" || typeof b === "string"){
-        return "parâmetros inválidos"
-    } else if (typeof b === "undefined") {
-        return "parâmetros inválidos"
-    } else {
-        return a + b
+    let numA = parseInt(a)
+    if (isNaN(numA)) {
+        return "Parâmetros Inválidos"
     }
+    let numB = parseInt(b)
+    if (isNaN(numB)) {
+        return "Parâmetros Inválidos"
+    }
+    return numA + numB
 }
+
+console.log(soma("joao", 2))
