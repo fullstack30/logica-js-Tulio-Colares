34b33349f4a08f64169ab821080ad2407469ef73
Tulio Colares <tuliocsaraiva@gmail.com>
2024-01-13 14:57:58 -0300

    trocando p/ parse float

--- src/soma.js
+++ src/soma.js
@@ -1,11 +1,13 @@
 function soma(a, b) {
-    let numA = parseInt(a)
+    let numA = parseFloat(a)
     if (isNaN(numA)) {
         return "Parâmetros Inválidos"
     }
-    let numB = parseInt(b)
+    let numB = parseFloat(b)
     if (isNaN(numB)) {
         return "Parâmetros Inválidos"
     }
     return numA + numB
 }
+
+console.log(soma("1.8" , 2.1))
