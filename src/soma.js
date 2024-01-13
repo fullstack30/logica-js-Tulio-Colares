b62976ae4d9b77a828d11b6f493ea076ae420df5
Tulio Colares <tuliocsaraiva@gmail.com>
2024-01-13 15:01:43 -0300

    corrigindo mensagem de parametros invalidos

--- src/soma.js
+++ src/soma.js
@@ -1,11 +1,11 @@
 function soma(a, b) {
     let numA = parseFloat(a)
     if (isNaN(numA)) {
-        return "Parâmetros Inválidos"
+        return "Parâmetros inválidos"
     }
     let numB = parseFloat(b)
     if (isNaN(numB)) {
-        return "Parâmetros Inválidos"
+        return "Parâmetros inválidos"
     }
     return numA + numB
 }
