3bc425615a23fda7b5dc3ff7c97350430f79d611
Tulio Colares <tuliocsaraiva@gmail.com>
2024-01-17 19:14:22 -0300

    primeira tentativa

---
+++ src/soma_impar.js
@@ -0,0 +1,17 @@
+function soma_impar(numInt) {
+    if (numInt <= 0) {
+        return "Função aceita apenas números inteiros positivos"
+    } else if (Number.isInteger(numInt == "false") ) {
+        return "Função aceita apenas números inteiros positivos"
+    }
+
+        let soma = 1
+        for (let i = 2; i <= numInt; i++){
+            if (i % 2 == 0) {
+                soma = soma
+            } else if (i % 2 !== 0) {
+                soma = soma + i
+            }
+        }
+        return soma
+}
