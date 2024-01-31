fc9034dc9186395ce3b2b51ae0e845213dae3a2f
Tulio <tuliocsaraiva@gmail.com>
2024-01-31 16:01:34 -0300

    vogais

---
+++ src/vogais.js
@@ -0,0 +1,17 @@
+function vogais(texto = "") {
+    if (typeof texto != 'string'){
+        return "Função aceita apenas string como argumento." 
+    }
+    if (texto == ""){
+        return 0
+    }
+    let contador = 0
+    for (let i = 0; i <= texto.length; i++) {
+        if (texto[i] == "a" || texto[i] == "e" || texto[i] == "i" || texto[i] == "o" || texto[i] == "u") {
+            contador++
+        }
+    }
+    return contador
+}
+
+console.log(vogais(45))
