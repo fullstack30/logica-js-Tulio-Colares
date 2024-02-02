6956bd65436287aa1be6a0af790c6c9d8fbaa8e4
Tulio <tuliocsaraiva@gmail.com>
2024-02-02 16:36:32 -0300

    adicionando casos do argumento

--- src/fatorial.js
+++ src/fatorial.js
@@ -1,4 +1,7 @@
 function fatorial(num) {
+    if (typeof num !== 'number' || num < 1 || num == null) {
+        "Função deve receber um argumento do tipo inteiro positivo."
+    }
     let result = num;
     if (num === 0 || num === 1) 
       return 1; 
