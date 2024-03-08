05950dba37fa247558869f55e6bb80564894bd2f
Tulio <tuliocsaraiva@gmail.com>
2024-03-08 13:59:55 -0300

    adicionando checagens

--- src/temperatura.js
+++ src/temperatura.js
@@ -1,15 +1,28 @@
 function temperatura(imp, out) {
     if (typeof imp != 'string' || imp.slice(-1) != "c" && imp.slice(-1) != "C" && imp.slice(-1) != "f" && imp.slice(-1) != "F") {
-        console.log('Temperatura inválida!')
+        return 'Temperatura inválida!'
+    } if (typeof out != 'string' || out != 'c' && out != 'C' && out != 'f' && out != 'F') {
+        return 'Temperatura inválida!'
     }
     if(imp.slice(-1) == "c" || imp.slice(-1) == "C") {
-        imp = imp.slice(0, -1)
-        let out = (imp * 9/5) + 32 
-        console.log(`${imp} graus célcius são ${out} graus fahrehnheit`)
+        if (out.toUpperCase() == imp.slice(-1).toUpperCase() ) {
+            return "Temperatura inválida!"
+        } else {
+            imp = imp.slice(0, -1)
+            let res = (imp * 9/5) + 32 
+            return  `${res}${out}`
+        }
+        
     } if (imp.slice(-1) == "f" || imp.slice(-1) == "F") {
-        imp = imp.slice(0, -1)
-        let out = (imp - 32) * 5/9  
-        console.log(`${imp} graus fahrehnheit são ${out} graus celsius`)
+        if (out.toUpperCase() == imp.slice(-1).toUpperCase() ) {
+            return "Temperatura inválida!"
+        } else if (out.toUpperCase() == imp.slice(-1).toUpperCase()) {
+            return "Temperatura inválida!"
+        } else {
+            imp = imp.slice(0, -1)
+            let res = (imp - 32) * 5/9  
+            return `${res}${out}`
+        }
     }
 }
 
